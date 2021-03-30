2f65341b9481e778315315687b88c2d6a3a06e57
Ryu Jae Young <qpig9302@naver.com>
2021-03-30 15:10:46 +0900

    uwu

--- base.py
+++ base.py
@@ -1,15 +1,17 @@
 from tkinter import *
 from tkinter import messagebox
 import tkinter as tk
+import pytube
 
 #변수 이름은 "홍윤석"씨가 추천해줌 내 의지 아님 ;;
 bybabo = Tk()
 bybabo.title("TEST")
-bybabo.geometry("640x280")
+bybabo.geometry("240x100")
 bybabo.resizable(False, False) 
 
 def onclick():
     bt["text"] = "잠시만 기다려요.."
+    pytube.YouTube(tk.Entry.get(input)).streams.get_highest_resolution().download('./YoutubeDownload')
     messagebox.showinfo("좋아요!", """다운로드에 성공했어요! 파일은 "./YoutubeDownload" 에 넣어둘께요!""")
     bt["text"] = "url을 입력해주세요!"
 
