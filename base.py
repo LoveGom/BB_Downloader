d0f3b1ddc2cd579b3a6d24daa863d33182db87a8
Ryu Jae Young <qpig9302@naver.com>
2021-04-07 19:55:38 +0900

    uwu

--- base.py
+++ base.py
@@ -21,14 +21,14 @@ bybabo.geometry("240x100") # 창 해상도 정의
 bybabo.resizable(False, False)  # 창 해상도 변경 금지 
 #bybabo.iconphoto(False, tk.PhotoImage(file=resource_path("./icon.png"))) # 아이콘 정의
 def onclick(): # 버튼을 클릭했을때 
-    bt["text"] = "잠시만 기다려요.." # 버튼 이름 바꾸기 + 시발 이거 왜이레 #2
+    bt["text"] = "잠시만 기다려요.." # 버튼 이름 바꾸기 
     pytube.YouTube(tk.Entry.get(input)).streams.get_highest_resolution().download('./YoutubeDownload') # Entrl 에 있는 값을 받은 후 유튜브 다운로드
     #uwu = tk.Entry.get(input).title 
     #pytube.Youtube(tk.Entry.get(input)).streams.filter(only_audio=True).first().download('./YouTubeDownload')
     #os.rename(f'.\YoutubeDownload\{uwu}.mp4',f'.\YoutubeDownload\{uwu}.mp3')
     #tk.Entry.get(input).streams.filter(res="1080p").first().download('./YouTubeDownload')
-    messagebox.showinfo("좋아요!", """다운로드에 성공했어요! 파일은 "./YoutubeDownload" 에 넣어둘께요!""") # 창 띄우기 3
-    bt["text"] = "슈우웅!" # 
+    messagebox.showinfo("좋아요!", """다운로드에 성공했어요! 파일은 "./YoutubeDownload" 에 넣어둘께요!""") # 창 띄우기 
+    bt["text"] = "슈우웅!" 
 text = Label(bybabo, text="url을 입력해주세요!") # 텍스트 속성 값
 input = Entry(bybabo) 
 bt = Button(bybabo, text="슈우웅!", command=onclick)
